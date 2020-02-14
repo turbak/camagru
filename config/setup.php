c55f9d1c9adefb3e02e48a29adf7945966200806
ishcherbina <ishcherbina@phoenixit.ru>
2020-02-14 17:24:01 +0300

    Added basic MVC Logic

--- config/setup.php
+++ config/setup.php
@@ -1,1 +1,6 @@
 <?php
+require_once 'application/core/model.php';
+require_once 'application/core/view.php';
+require_once 'application/core/controller.php';
+require_once 'application/core/route.php';
+Route::start();
--- index.php
+++ index.php
@@ -1,1 +1,3 @@
 <?php
+ini_set('display_errors', 1);
+require_once 'config/setup.php';
